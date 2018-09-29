b9c73fbbac4b20fd5304df885514900998730f8e
Snowbird <jakounter@yahoo.com>
2018-09-29 13:26:14 -0500

    routes updated

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,17 @@
 const router = require("express").Router();
-const bookRoutes = require("./books");
+const billRoutes = require("./bills");
+const contractRoutes = require("./contracts");
+const messageRoutes = require("./messages");
+const roomRoutes = require("./rooms");
+const todoRoutes = require("./todos");
+const userRoutes = require("./users");
 
 // Book routes
-router.use("/books", bookRoutes);
+router.use("/bills", billRoutes);
+router.use("/contracts", contractRoutes);
+router.use("/messages", messageRoutes);
+router.use("/rooms", roomRoutes);
+router.use("/todos", todoRoutes);
+router.use("/users", userRoutes);
 
 module.exports = router;
