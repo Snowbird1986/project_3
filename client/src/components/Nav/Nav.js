12884f7165942687e23e851a575b5744899bbe50
Snowbird <jakounter@yahoo.com>
2018-10-02 18:30:41 -0500

    a

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,11 +1,62 @@
-import React from "react";
+import React, { Component } from "react";
+import Login from "../Facebook/Login";
+import { Col, Row, Container } from "../../components/Grid";
 
-const Nav = () => (
-  <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-    <a className="navbar-brand" href="/">
-      React Reading List
-    </a>
-  </nav>
-);
+class Nav extends Component {
+  state = {
+    username: null,
+  }
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+  handleFormSubmit = event => {
+    event.preventDefault();
+  }
+  onFacebookLogin = (loginStatus, resultObject) => {
+    if (loginStatus === true) {
+      this.setState({
+        username: resultObject.user.name
+      });
+    } else {
+      alert('Facebook login error');
+    }
+  }
+
+
+
+render() {
+  return (
+    <Container fluid>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <Row>
+          <a className="navbar-brand" href="/">
+            <div className="App">
+              <header className="App-header">
+                <h1 className="App-title"> Media Login</h1>
+              </header>
+              <div className="App-intro">
+                {!this.state.username &&
+                  <div>
+                    <p>Click on one of any button below to login</p>
+                    <Login onLogin={this.onFacebookLogin}>
+                      <button>Facebook</button>
+                    </Login>
+                  </div>
+                }
+                {this.state.username &&
+                  <p>Welcome back, {this.state.username}</p>
+                }
+              </div>
+            </div>
+          </a>
+        </Row>
+      </nav>
+    </Container>
+  );
+}
+}
 
 export default Nav;
