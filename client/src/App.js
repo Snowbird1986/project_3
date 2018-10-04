59045b621ff05fd6ff75eff49218a8df10be26ef
Snowbird <jakounter@yahoo.com>
2018-10-04 18:56:10 -0500

    a

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,10 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Books from "./pages/Books";
 import User from "./pages/Users";
+import UserSearch from "./pages/UserSearch";
 import Landing from "./pages/Landing";
 import Room from "./pages/Rooms";
+import RoomSearch from "./pages/RoomSearch";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
@@ -17,6 +19,8 @@ const App = () => (
         <Route exact path="/" component={Landing} />
         <Route exact path="/user" component={User} />
         <Route exact path="/room" component={Room} />
+        <Route exact path="/userSearch" component={UserSearch} />
+        <Route exact path="/roomSearch" component={RoomSearch} />
         {/* <Route exact path="/books" component={Books} />
         <Route exact path="/books/:id" component={Detail} /> */}
         <Route component={NoMatch} /> */}
