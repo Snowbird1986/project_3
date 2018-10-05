c4cafe46636b26209d046492c603c2e80fabe280
Snowbird <jakounter@yahoo.com>
2018-10-04 20:18:51 -0500

    a

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import User from "./pages/Users";
+import UserHome from "./pages/UserHome";
 import UserSearch from "./pages/UserSearch";
 import Landing from "./pages/Landing";
 import Room from "./pages/Rooms";
@@ -21,13 +22,13 @@ const App = () => (
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route exact path="/user" component={User} />
+        <Route exact path="/userHome" component={UserHome} />
         <Route exact path="/room" component={Room} />
         <Route exact path="/userSearch" component={UserSearch} />
         <Route exact path="/roomSearch" component={RoomSearch} />
         <Route exact path="/roomCreate" component={RoomCreate} />
         <Route exact path="/todoCreate" component={TodoCreate} />
         <Route exact path="/billCreate" component={BillCreate} />
-
         <Route component={NoMatch} />
       </Switch>
       <Footer />
