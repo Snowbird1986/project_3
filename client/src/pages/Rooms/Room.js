36e4fca2898ac5a855a0356ef8fae0b383215aa7
Bailey Jace <baileyjace@gmail.com>
2018-10-04 21:00:18 -0500

    v basic room

--- client/src/pages/Rooms/Room.js
+++ client/src/pages/Rooms/Room.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../../components/Grid";
+import { FormBtn, Input, TextArea } from "../../components/Form";
 import Jumbotron from "../../components/Jumbotron";
 import API from "../../utils/API";
 
@@ -10,7 +11,32 @@ class Room extends Component {
     render() {
         return (
     <Container fluid>
-            Room
+            <Row>
+                <Col size="md-12">
+                <Jumbotron>
+                    <h1>Room</h1>
+                </Jumbotron>
+                <Row>
+                    <div class="list-group">
+                    <a href="/message" class="list-group-item list-group-item-action">
+                        Messages
+                    </a>
+                    <a href="/bill" class="list-group-item list-group-item-action">
+                        Bills
+                    </a>
+                    <a href="/user" class="list-group-item list-group-item-action">
+                        Users
+                    </a>
+                    <a href="/todo" class="list-group-item list-group-item-action">
+                        To Dos
+                    </a>
+                    <a href="/contract" class="list-group-item list-group-item-action">
+                        Contracts
+                    </a>
+                    </div>
+                </Row>
+                </Col>
+            </Row>
     </Container>
         )}
 }
